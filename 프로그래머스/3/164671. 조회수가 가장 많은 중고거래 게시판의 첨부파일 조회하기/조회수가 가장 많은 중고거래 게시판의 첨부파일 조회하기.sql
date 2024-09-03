51cc79470c9a6069a6b54ec06f5c1f20d48e07f0
dongjaee <154665357+dongjaee@users.noreply.github.com>
2024-09-03 14:14:50 +0000

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,8 @@
+select concat('/home/grep/src/',a.BOARD_ID,'/',FILE_ID,FILE_NAME,FILE_EXT) as FILE_PATH
+from USED_GOODS_BOARD as a
+join USED_GOODS_FILE as b on a.BOARD_ID=b.BOARD_ID
+where a.VIEWS = (select max(VIEWS) from USED_GOODS_BOARD)
+order by b.FILE_ID DESC
+
+
+
