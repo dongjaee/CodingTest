2274f784ca0a9a10a80e6570fcc810ad2b8f6eee
dongjaee <154665357+dongjaee@users.noreply.github.com>
2024-10-18 11:21:22 +0000

    [level 2] Title: 타겟 넘버, Time: 691.55 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+def solution(numbers, target):
+    answer=dfs(numbers,target,0)
+    return answer
+    
+def dfs(numbers,target,depth):
+    answer=0
+    if depth==len(numbers):
+        if sum(numbers)==target:
+            return 1
+        else: return 0
+    else:
+        answer+=dfs(numbers,target,depth+1)
+        numbers[depth]*=-1
+        answer+=dfs(numbers,target,depth+1)
+        return answer
