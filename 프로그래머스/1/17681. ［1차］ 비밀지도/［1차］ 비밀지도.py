d604ee50a7936d854a2f96c21fdab8be518ad2b1
dongjaee <154665357+dongjaee@users.noreply.github.com>
2024-10-28 11:58:36 +0000

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,13 +1,8 @@
-#1.이진수 변환
-#2.해당 자리수에 있는거면 cnt+=1
-    #cnt*# answer 어팬드
-
-
 def solution(n, arr1, arr2):
     answer = []
     for a, b in zip(arr1, arr2):
-        a = bin(a)[2:].zfill(n)
-        b = bin(b)[2:].zfill(n)
+        a = bin(a)[2:].rjust(n,'0')
+        b = bin(b)[2:].rjust(n,'0')
         
         combined = ''.join(['#' if x == '1' or y == '1' else ' ' for x, y in zip(a, b)])
         
