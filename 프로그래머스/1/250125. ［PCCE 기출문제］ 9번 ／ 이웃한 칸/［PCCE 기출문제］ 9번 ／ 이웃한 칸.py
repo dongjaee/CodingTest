7eddaa8241b7b299fd908b1275d3e8fba1d3cc21
dongjaee <154665357+dongjaee@users.noreply.github.com>
2024-10-29 14:53:40 +0000

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,18 +1,12 @@
 def solution(board, h, w):
-    start = board[h][w]
-    cnt = 0  # 인접한 같은 색깔 칸의 개수를 저장
-
-    # 상하좌우 방향 설정
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-    # 각 방향을 탐색
-    for dh, dw in directions:
-        nh, nw = h + dh, w + dw
-
-        # 인접 칸이 보드의 범위를 벗어나지 않는지 확인
-        if 0 <= nh < len(board) and 0 <= nw < len(board[0]):
-            # 인접 칸의 색상이 시작 칸과 같으면 카운트 증가
-            if board[nh][nw] == start:
-                cnt += 1
-
-    return cnt
+    answer = 0
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    color = board[h][w]
+    for i in range(len(dx)):
+        nx = h + dx[i]
+        ny = w + dy[i]
+        if 0 <= nx < len(board) and 0 <= ny < len(board):
+            if board[nx][ny] == color:
+                answer += 1
+    return answer
